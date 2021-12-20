843285a0a7934cbf5a7b4202d0009c6523ad208e
pyrhaa <sboubtane@protonmail.com>
2021-12-20 23:26:52 +0100

    put all we need in Course compo

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,40 +1,5 @@
 import React from 'react';
 
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
-};
-
-// arrayPart : catch the array parts in the props object
-// parts : map to arrayPart each element name and exercices in a list
-const Part = (props) => {
-  const arrayPart = props.parts;
-
-  const parts = arrayPart.map((el) => {
-    return <li key={el.name}>{el.name + ' ' + el.exercises}</li>;
-  });
-
-  return <>{parts}</>;
-};
-
-const Content = (props) => {
-  return (
-    <div>
-      <p>
-        <Part parts={props.parts} />
-      </p>
-    </div>
-  );
-};
-
-// exercices : catch all exercices properties from array parts in props object
-// [first, second, third] : take each exercices number values individualy
-const Total = (props) => {
-  const exercices = props.parts.map((el) => el.exercises);
-  const [first, second, third] = exercices;
-  const sum = first + second + third;
-  return <p>Number of exercises {sum}</p>;
-};
-
 const App = () => {
   const course = {
     id: 1,
