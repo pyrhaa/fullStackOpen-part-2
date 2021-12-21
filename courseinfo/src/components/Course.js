a5b6e948be97f25b2e5c739989a69914ab370db3
pyrhaa <sboubtane@protonmail.com>
2021-12-21 13:44:57 +0100

    refactoring the code in Course compo

--- courseinfo/src/components/Course.js
+++ courseinfo/src/components/Course.js
@@ -1,34 +1,49 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
+import React from 'react';
+
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
 };
 
 // arrayPart : catch the array parts in the props object
 // parts : map to arrayPart each element name and exercices in a list
-const Part = (props) => {
-  const arrayPart = props.parts;
+const Part = ({ course }) => {
+  const arrayPart = course.parts;
 
-  const parts = arrayPart.map((el) => {
-    return <li key={el.name}>{el.name + ' ' + el.exercises}</li>;
-  });
+  // const parts = arrayPart.map((el) => {
+  //   return <li key={el.id}>{el.name + ' ' + el.exercises}</li>;
+  // });
 
-  return <>{parts}</>;
+  return (
+    <>
+      <p>ok</p>
+    </>
+  );
 };
 
-const Content = (props) => {
+const Content = ({ course }) => {
   return (
     <div>
-      <p>
-        <Part parts={props.parts} />
-      </p>
+      <Part course={course} />
     </div>
   );
 };
 
 // exercices : catch all exercices properties from array parts in props object
 // [first, second, third] : take each exercices number values individualy
-const Total = (props) => {
-  const exercices = props.parts.map((el) => el.exercises);
-  const [first, second, third] = exercices;
-  const sum = first + second + third;
-  return <p>Number of exercises {sum}</p>;
+// const Total = (props) => {
+//   const exercices = props.parts.map((el) => el.exercises);
+//   const [first, second, third] = exercices;
+//   const sum = first + second + third;
+//   return <p>Number of exercises {sum}</p>;
+// };
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course} />
+      <Content course={course} />
+    </>
+  );
 };
+
+export default Course;
