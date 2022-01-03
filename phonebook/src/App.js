8d822b769b7e3b523b776ad01be327245e76f2c2
pyrhaa <sboubtane@protonmail.com>
2022-01-03 15:42:33 +0100

    display a search input like mdn exemple

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '070809', id: 1 },
-    { name: 'Azul Fella', number: '12345', id: 2 }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]);
   const [newName, setNewName] = useState('');
   const [newPhone, setNewPhone] = useState('');
@@ -41,7 +43,18 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        <label htmlFor="site-search">filter shown with</label>
+        <input
+          type="search"
+          id="site-search"
+          name="search"
+          aria-label="Search through site content"
+        />
+        <button>Search</button>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleChangeName} />
