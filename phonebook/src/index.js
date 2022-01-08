61c64519d86d9d7be2e4551640c98c9f089c8899
pyrhaa <sboubtane@protonmail.com>
2022-01-08 14:55:21 +0100

    remove css in index html for no json server error

--- phonebook/src/index.js
+++ phonebook/src/index.js
@@ -1,4 +1,10 @@
 import ReactDOM from 'react-dom';
 import App from './App';
+import axios from 'axios';
+
+axios.get('http://localhost:3001/persons').then((res) => {
+  const persons = res.data;
+  console.log(persons);
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
