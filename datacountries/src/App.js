5be0803f646fa23506c5db793fe4f84763531ab5
pyrhaa <sboubtane@protonmail.com>
2022-01-10 13:23:21 +0100

    data name fetching of one country ok

--- datacountries/src/App.js
+++ datacountries/src/App.js
@@ -2,18 +2,30 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Filter from './components/Filter';
 
+const CountryList = ({ search, display, country }) => {
+  return (
+    <div>
+      <h2>Azul</h2>
+      <p>{country}</p>
+    </div>
+  );
+};
+
 const App = () => {
   const [search, setSearch] = useState('');
+  const [country, setCountry] = useState('');
+  const [display, setDisplay] = useState();
 
   useEffect(() => {
-    axios
-      .get('https://restcountries.com/v3.1/all')
-      .then((res) => console.log(res.data));
+    axios.get('https://restcountries.com/v3.1/all').then((res) => {
+      return setCountry(res.data[0].name.common);
+    });
   }, []);
 
   return (
     <div>
       <Filter handleChangeSearch={(e) => setSearch(e.target.value.trim())} />
+      <CountryList search={search} display={display} country={country} />
     </div>
   );
 };
