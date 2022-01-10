0e7129cd928f885dadf22d8039fc47968eef6157
pyrhaa <sboubtane@protonmail.com>
2022-01-10 12:38:36 +0100

    use useEffect & axios for fetch data

--- datacountries/src/App.js
+++ datacountries/src/App.js
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Filter from './components/Filter';
 
 const App = () => {
   const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then((res) => console.log(res.data));
+  }, []);
+
   return (
     <div>
       <Filter handleChangeSearch={(e) => setSearch(e.target.value.trim())} />
