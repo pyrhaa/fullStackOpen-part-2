6199f1731d5c293e7d7d2fdd56e91e792c8a5cd2
pyrhaa <sboubtane@protonmail.com>
2022-01-10 15:45:02 +0100

    var filter each country and empty array in country state to avoid error message about useEffect

--- datacountries/src/App.js
+++ datacountries/src/App.js
@@ -2,30 +2,36 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Filter from './components/Filter';
 
-const CountryList = ({ search, display, country }) => {
+const CountryList = ({ search, country }) => {
+  console.log(country);
+
+  const filterCountries = country.filter(
+    (el) => el.name.common.toLowerCase().indexOf(search.toLowerCase()) !== -1
+  );
+
   return (
     <div>
       <h2>Azul</h2>
-      <p>{country}</p>
+      <p>hey</p>
     </div>
   );
 };
 
 const App = () => {
   const [search, setSearch] = useState('');
-  const [country, setCountry] = useState('');
-  const [display, setDisplay] = useState();
+  const [country, setCountry] = useState([]);
+  const [showList, setShowList] = useState(false);
 
   useEffect(() => {
     axios.get('https://restcountries.com/v3.1/all').then((res) => {
-      return setCountry(res.data[0].name.common);
+      return setCountry(res.data);
     });
   }, []);
 
   return (
     <div>
       <Filter handleChangeSearch={(e) => setSearch(e.target.value.trim())} />
-      <CountryList search={search} display={display} country={country} />
+      <CountryList search={search} country={country} />
     </div>
   );
 };
