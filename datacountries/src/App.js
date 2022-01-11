c8fb17125daf0516b65c456a1dcbe489bb44d0d9
pyrhaa <sboubtane@protonmail.com>
2022-01-11 18:34:32 +0100

    countryInfo compo crete to render info of one country and resolvation of the undefined first render of the useEffect

--- datacountries/src/App.js
+++ datacountries/src/App.js
@@ -2,39 +2,55 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Filter from './components/Filter';
 
-const CountryList = ({ search, country }) => {
-  const filterCountries = country.filter(
-    (el) => el.name.common.toLowerCase().indexOf(search.toLowerCase()) !== -1
-  );
+// const CountryList = ({ search, country }) => {
+//   const filterCountries = country.filter(
+//     (el) => el.name.common.toLowerCase().indexOf(search.toLowerCase()) !== -1
+//   );
 
-  console.log(filterCountries);
+//   console.log(filterCountries);
 
-  if (filterCountries.length > 10 && search.length > 0) {
-    return <p>Be more specific, please, too many matches.</p>;
-  } else if (search.length === 0 && filterCountries.length > 10) {
-    return <p>Where we going today ?</p>;
-  } else if (filterCountries.length === 10) {
-    return (
+//   if (filterCountries.length > 10 && search.length > 0) {
+//     return <p>Be more specific, please, too many matches.</p>;
+//   } else if (search.length === 0 && filterCountries.length > 10) {
+//     return <p>Where we going today ?</p>;
+//   } else if (filterCountries.length === 10) {
+//     return (
+//       <ul>
+//         {filterCountries.map((el) => {
+//           const id = el.ccn3;
+//           return <li key={parseInt(id)}>{el.name.common}</li>;
+//         })}
+//       </ul>
+//     );
+//   } else {
+//     return (
+//       <CountryInfo
+//         search={search}
+//         country={country}
+//         filterCountries={filterCountries}
+//       />
+//     );
+//   }
+// };
+
+const CountryInfo = ({ country }) => {
+  const pays = country[1];
+  const paysLang = Object.values(pays.languages);
+  console.log(country);
+  return (
+    <div>
+      <h1>{pays.name.common}</h1>
+      <p>capital {pays.capital}</p>
+      <p>population {pays.population}</p>
+      <h3>languages</h3>
       <ul>
-        {filterCountries.map((el) => {
-          const id = el.ccn3;
-          return <li key={parseInt(id)}>{el.name.common}</li>;
+        {paysLang.map((el, i) => {
+          return <li key={el[i]}>{el}</li>;
         })}
       </ul>
-    );
-  } else {
-    return (
-      <CountryInfo
-        search={search}
-        country={country}
-        filterCountries={filterCountries}
-      />
-    );
-  }
-};
-
-const CountryInfo = ({ search, country, filterCountries }) => {
-  return <p>Azul</p>;
+      <img style={{ width: '100px' }} src={pays.flags.png} />
+    </div>
+  );
 };
 
 const App = () => {
@@ -50,7 +66,7 @@ const App = () => {
   return (
     <div>
       <Filter handleChangeSearch={(e) => setSearch(e.target.value.trim())} />
-      <CountryList search={search} country={country} />
+      {country.length > 0 && <CountryInfo country={country} />}
     </div>
   );
 };
