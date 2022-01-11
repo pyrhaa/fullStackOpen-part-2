7560689a6cdd2efe52e98f7d9e92e0717805ce68
pyrhaa <sboubtane@protonmail.com>
2022-01-11 11:34:18 +0100

    countryList display condition ok

--- datacountries/src/App.js
+++ datacountries/src/App.js
@@ -3,26 +3,31 @@ import axios from 'axios';
 import Filter from './components/Filter';
 
 const CountryList = ({ search, country }) => {
-  console.log(country);
-
   const filterCountries = country.filter(
     (el) => el.name.common.toLowerCase().indexOf(search.toLowerCase()) !== -1
   );
 
-  return (
-    <ul>
-      {filterCountries.map((el) => {
-        const id = el.ccn3;
-        return <li key={parseInt(id)}>{el.name.common}</li>;
-      })}
-    </ul>
-  );
+  console.log(filterCountries);
+
+  if (filterCountries.length > 10 && search.length > 0) {
+    return <p>Be more specific, please, too many matches.</p>;
+  } else if (search.length === 0 && filterCountries.length > 10) {
+    return <p>Where we going today ?</p>;
+  } else {
+    return (
+      <ul>
+        {filterCountries.map((el) => {
+          const id = el.ccn3;
+          return <li key={parseInt(id)}>{el.name.common}</li>;
+        })}
+      </ul>
+    );
+  }
 };
 
 const App = () => {
   const [search, setSearch] = useState('');
   const [country, setCountry] = useState([]);
-  const [showList, setShowList] = useState(false);
 
   useEffect(() => {
     axios.get('https://restcountries.com/v3.1/all').then((res) => {
