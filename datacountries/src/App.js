90e912f4f5b8b7b5ed8bc33b23b15d1eb07cc838
pyrhaa <sboubtane@protonmail.com>
2022-01-14 14:20:57 +0100

    step 2 ok

--- datacountries/src/App.js
+++ datacountries/src/App.js
@@ -6,7 +6,6 @@ import CountryList from './components/CountryList';
 const App = () => {
   const [search, setSearch] = useState('');
   const [country, setCountry] = useState([]);
-  const [show, setShow] = useState(false);
 
   useEffect(() => {
     axios.get('https://restcountries.com/v3.1/all').then((res) => {
