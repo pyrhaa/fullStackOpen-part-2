ac79462b6a6ed2d27f005765fdab656f62ffda70
pyrhaa <sboubtane@protonmail.com>
2022-01-14 18:55:11 +0100

    datacountries end

--- datacountries/src/components/CountryInfo.js
+++ datacountries/src/components/CountryInfo.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const CountryInfo = ({ arrayObject }) => {
-  const [weatherInfo, setWeatherInfo] = useState({});
+  const [weatherInfo, setWeatherInfo] = useState();
   const theCountry = arrayObject[0];
   const countryLang = Object.values(theCountry.languages);
   const weatherApi = process.env.REACT_APP_API_KEY;
@@ -17,32 +17,34 @@ const CountryInfo = ({ arrayObject }) => {
       });
   }, []);
 
-  console.log(weatherInfo);
-
-  return (
-    <div>
-      <h1>{theCountry.name.common}</h1>
-      <p>capital {theCountry.capital}</p>
-      <p>population {theCountry.population}</p>
-      <h3>languages</h3>
-      <ul>
-        {countryLang.map((el) => {
-          return <li key={Math.random()}>{el}</li>;
-        })}
-      </ul>
-      <img style={{ width: '100px' }} src={theCountry.flags.png} />
-      <h3>Weather in {theCountry.name.common}</h3>
-      <p>temperature: {weatherInfo.current.temperature} Celsius</p>
-      <img
-        style={{ width: '50px' }}
-        src={weatherInfo.current.weather_icons[0]}
-      />
-      <p>
-        wind: {weatherInfo.current.wind_speed} mph direction{' '}
-        {weatherInfo.current.wind_dir}
-      </p>
-    </div>
-  );
+  if (weatherInfo !== undefined) {
+    return (
+      <div>
+        <h1>{theCountry.name.common}</h1>
+        <p>capital {theCountry.capital}</p>
+        <p>population {theCountry.population}</p>
+        <h3>languages</h3>
+        <ul>
+          {countryLang.map((el) => {
+            return <li key={Math.random()}>{el}</li>;
+          })}
+        </ul>
+        <img style={{ width: '100px' }} src={theCountry.flags.png} />
+        <h3>Weather in {theCountry.name.common}</h3>
+        <p>temperature: {weatherInfo.current.temperature} Celsius</p>
+        <img
+          style={{ width: '50px' }}
+          src={weatherInfo.current.weather_icons[0]}
+        />
+        <p>
+          wind: {weatherInfo.current.wind_speed} mph direction{' '}
+          {weatherInfo.current.wind_dir}
+        </p>
+      </div>
+    );
+  } else {
+    return <p>Loading ...</p>;
+  }
 };
 
 export default CountryInfo;
