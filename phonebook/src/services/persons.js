02b0744d86a004b3edd316d67b2bfebea432816c
pyrhaa <sboubtane@protonmail.com>
2022-01-22 15:01:03 +0100

    build end

--- phonebook/src/services/persons.js
+++ phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'https://phonebook-fsopen2021.herokuapp.com/api/persons';
+const baseUrl = '/api/persons';
 
 const getAll = () => {
   return axios.get(baseUrl);
