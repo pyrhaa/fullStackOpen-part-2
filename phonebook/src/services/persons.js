06e0056e8dd223294da9dd67e0817838bf79cce4
pyrhaa <sboubtane@protonmail.com>
2022-01-27 22:54:57 +0100

    some trouble, front uncaught typeError...

--- phonebook/src/services/persons.js
+++ phonebook/src/services/persons.js
@@ -2,19 +2,23 @@ import axios from 'axios';
 const baseUrl = '/api/persons';
 
 const getAll = () => {
-  return axios.get(baseUrl);
+  const request = axios.get(baseUrl);
+  return request.then((res) => res.data);
 };
 
 const create = (newObject) => {
-  return axios.post(baseUrl, newObject);
+  const request = axios.post(baseUrl, newObject);
+  return request.then((res) => res.data);
 };
 
 const update = (id, newObject) => {
-  return axios.put(`${baseUrl}/${id}`, newObject);
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then((res) => res.data);
 };
 
 const deletePost = (id) => {
-  return axios.delete(`${baseUrl}/${id}`);
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((res) => res.data);
 };
 
 export default { getAll, create, update, deletePost };
